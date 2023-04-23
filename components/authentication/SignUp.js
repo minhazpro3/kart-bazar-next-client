b049ca60691ebf4504c80381e8b13f6d6f41bafc
minhazpro3 <minhazpro1@gmail.com>
2023-04-23 17:55:00 +0600

    added flex box

--- components/authentication/SignUp.js
+++ components/authentication/SignUp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import Head_cos from "./../share/Head_cos";
+import Image from "next/image";
 
 const SignUp = () => {
   const {
@@ -10,42 +11,61 @@ const SignUp = () => {
   } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <div className="container_signup_component">
+    <div>
       <Head_cos />
-      <div className="signup_component">
-        <div>{/* images */}</div>
+      <div className="container_signup_component">
         <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="input_group">
-              <input
-                {...register("firstName", { required: true, maxLength: 20 })}
-                aria-invalid={errors.firstName ? "true" : "false"}
+          <div className="signup_component">
+            <div className="signup_img">
+              <Image
+                src="https://i.ibb.co/mX3wQDJ/sign-up.png"
+                alt="signup"
+                fill
               />
-              {/* {errors.firstName?.type === "required" && (
+            </div>
+            <div className="inputs_group">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="input_group">
+                  <input
+                    {...register("firstName", {
+                      required: true,
+                      maxLength: 20,
+                    })}
+                    aria-invalid={errors.firstName ? "true" : "false"}
+                  />
+                  {/* {errors.firstName?.type === "required" && (
                 <p role="alert">First name is required</p>
               )} */}
-              <label>Full Name</label>
+                  <label>Full Name</label>
+                </div>
+                <div className="input_group">
+                  <input {...register("email", { pattern: /^[A-Za-z]+$/i })} />
+                  <label>Email Address</label>
+                </div>
+                <div className="input_group">
+                  <input
+                    type="number"
+                    {...register("age", { min: 18, max: 99 })}
+                  />
+                  <label>Password</label>
+                </div>
+                <div className="input_group">
+                  <input
+                    type="number"
+                    {...register("age", { min: 18, max: 99 })}
+                  />
+                  <label>Confirm Password</label>
+                </div>
+                <div className="terms_condition">
+                  <label>
+                    <input type="checkbox" required /> I agree with Terms and
+                    Privacy{" "}
+                  </label>
+                </div>
+                <input type="submit" className="button" value="Signup" />
+              </form>
             </div>
-            <div className="input_group">
-              <input {...register("email", { pattern: /^[A-Za-z]+$/i })} />
-              <label>Email Address</label>
-            </div>
-            <div className="input_group">
-              <input type="number" {...register("age", { min: 18, max: 99 })} />
-              <label>Password</label>
-            </div>
-            <div className="input_group">
-              <input type="number" {...register("age", { min: 18, max: 99 })} />
-              <label>Confirm Password</label>
-            </div>
-            <div className="terms_condition">
-              <label>
-                <input type="checkbox" required /> I agree with Terms and
-                Privacy{" "}
-              </label>
-            </div>
-            <input type="submit" className="button" value="Signup" />
-          </form>
+          </div>
         </div>
       </div>
     </div>
