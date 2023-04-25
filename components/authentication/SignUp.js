aa5682bd7eaca7536e45d7928bcf9a00d66e7c9e
minhazpro3 <minhazpro1@gmail.com>
2023-04-25 11:42:47 +0600

    added & styled signup page

--- components/authentication/SignUp.js
+++ components/authentication/SignUp.js
@@ -4,6 +4,9 @@ import Head_cos from "./../share/Head_cos";
 import Image from "next/image";
 import Cart_banner from "../cart/Cart_banner";
 import Footer from "./../share/Footer";
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook } from "react-icons/bs";
+import Link from "next/link";
 
 const SignUp = () => {
   const {
@@ -34,68 +37,100 @@ const SignUp = () => {
                 />
               </div>
             </div>
-
-            {/* signup form */}
-            <div className="inputs_group">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form_title">
-                  <h2>Welcome To Kart Bazar</h2>
-                  <h5>Create a new account</h5>
-                </div>
-                {/* input group 1 */}
-                <div className="input_group">
-                  <input
-                    {...register("name", {
-                      required: true,
-                      maxLength: 20,
-                    })}
-                    name="name"
-                    required
-                    aria-invalid={errors.firstName ? "true" : "false"}
-                  />
-                  {/* {errors.firstName?.type === "required" && (
+            <div style={{ backgroundColor: " rgba(241, 235, 235, 0.686)", padding:"20px" }}>
+              {/* signup form */}
+              <div className="inputs_group">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="form_title">
+                    <h2>Welcome To Kart Bazar</h2>
+                    <h5>Create a new account</h5>
+                  </div>
+                  {/* input group 1 */}
+                  <div className="input_group">
+                    <input
+                      {...register("name", {
+                        required: true,
+                        maxLength: 20,
+                      })}
+                      name="name"
+                      required
+                      aria-invalid={errors.firstName ? "true" : "false"}
+                    />
+                    {/* {errors.firstName?.type === "required" && (
                 <p role="alert">First name is required</p>
               )} */}
-                  <label for="name">Full Name</label>
+                    <label for="name">Full Name</label>
+                  </div>
+                  {/* input group 2 */}
+                  <div className="input_group">
+                    <input
+                      {...register("email", { pattern: /^[A-Za-z]+$/i })}
+                      name="email"
+                      required
+                    />
+                    <label for="email">Email Address</label>
+                  </div>
+                  {/* input group 3 */}
+                  <div className="input_group">
+                    <input
+                      type="password"
+                      {...register("age", { min: 18, max: 99 })}
+                      name="password"
+                      required
+                    />
+                    <label for="password">Password</label>
+                  </div>
+                  {/* input group 4 */}
+                  <div className="input_group">
+                    <input
+                      type="password"
+                      {...register("age", { min: 18, max: 99 })}
+                      name="confirm password"
+                      required
+                    />
+                    <label for="confirm password">Confirm Password</label>
+                  </div>
+                  {/* input group 5 */}
+                  <div className="terms_condition">
+                    <label>
+                      <input
+                        style={{ color: "green" }}
+                        type="checkbox"
+                        required
+                      />{" "}
+                      I agree with <span>Terms</span> and <span>Privacy</span>
+                    </label>
+                  </div>
+                  <input type="submit" className="button" value="Signup" />
+                </form>
+              </div>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <div className="third_auth">
+                  <ul>
+                    <hr />
+                    <span>OR</span>
+                  </ul>
+                  <button className="third_button">
+                    {" "}
+                    <FcGoogle style={{ margin: "0 6px " }} /> Sign up with
+                    Google{" "}
+                  </button>
+                  <button className="third_button">
+                    {" "}
+                    <BsFacebook
+                      style={{ margin: "0 6px ", color: "blue" }}
+                    />{" "}
+                    Sign up with Facebook{" "}
+                  </button>
+                  <h5>
+                    Already have an account?
+                    <span>
+                      {" "}
+                      <Link href="auth/signin">Login</Link>
+                    </span>
+                  </h5>
                 </div>
-                {/* input group 2 */}
-                <div className="input_group">
-                  <input
-                    {...register("email", { pattern: /^[A-Za-z]+$/i })}
-                    name="email"
-                    required
-                  />
-                  <label for="email">Email Address</label>
-                </div>
-                {/* input group 3 */}
-                <div className="input_group">
-                  <input
-                    type="password"
-                    {...register("age", { min: 18, max: 99 })}
-                    name="password"
-                    required
-                  />
-                  <label for="password">Password</label>
-                </div>
-                {/* input group 4 */}
-                <div className="input_group">
-                  <input
-                    type="password"
-                    {...register("age", { min: 18, max: 99 })}
-                    name="confirm password"
-                    required
-                  />
-                  <label for="confirm password">Confirm Password</label>
-                </div>
-                {/* input group 5 */}
-                <div className="terms_condition">
-                  <label>
-                    <input type="checkbox" required /> I agree with Terms and
-                    Privacy{" "}
-                  </label>
-                </div>
-                <input type="submit" className="button" value="Signup" />
-              </form>
+              </div>
             </div>
           </div>
         </div>
